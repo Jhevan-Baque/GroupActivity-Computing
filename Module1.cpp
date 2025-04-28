894e9810ddea481dd4abb7753a4f867f50fe3e6d
Jhevan Baque <aerovan.dev@gmail.com>
2025-04-28 21:48:01 +0800

    Update Module1.cpp

--- Module1.cpp
+++ Module1.cpp
@@ -74,46 +74,47 @@ void decorateVenue() {
     }
 }
 
-void prepareFood() {
-    cout << "Preparing Food (Concurrent - Alternating steps):" << endl;
+void prepareFoodCooking() {
     string CookingSteps[3] = {"1. Cooking main dish", "2. Preparing appetizers", "3. Making desserts"};
-    string PlatingSteps[3] = {"1. Setting plates", "2. Garnishing dishes", "3. Arranging on tables"};
-
-    int i = 0;
-    bool isCookingTurn = true;
-
-    while (i < 3) {
-        if (isCookingTurn) {
-            cout << CookingSteps[i] << endl;
-            isCookingTurn = false;
-        } else {
-            cout << PlatingSteps[i] << endl;
-            isCookingTurn = true;
-            i++;
-        }
+    for (int i = 0; i < 3; i++) {
+        cout << CookingSteps[i] << endl;
         this_thread::sleep_for(chrono::milliseconds(200));
     }
 }
 
-void organizeGames() {
-    string Steps[3] = {"1. Setting up games area", "2. Preparing game materials", "3. Briefing participants"};
-    for (int i = 0; i < 3; i++) {
-        cout << Steps[i] << endl;
-        this_thread::sleep_for(chrono::milliseconds(200));
+void checkProgress() {
+    string decorationProgress[3] = {"1. Checking progress on decorations", "2. Checking progress on tables", "3. Checking progress on centerpieces"};
+    string foodProgress[3] = {"1. Checking progress on food preparation", "2. Checking progress on main dishes", "3. Checking progress on desserts"};
+
+    int i = 0;
+    bool isDecorationTurn = true;
+
+    while (i < 3) {
+        if (isDecorationTurn) {
+            cout << decorationProgress[i] << endl;
+            isDecorationTurn = false; 
+        } else {
+            cout << foodProgress[i] << endl;
+            isDecorationTurn = true; 
+            i++;
+        }
+        this_thread::sleep_for(chrono::milliseconds(500)); 
     }
 }
 
 void organizeParty() {
     cout << "Organizing Party (Concurrent + Parallel):" << endl;
 
-    thread t1(decorateVenue);
-    thread t2(prepareFood);
-    thread t3(organizeGames);
-
+    thread t1(decorateVenue); // Parallel
+    thread t2(prepareFoodCooking);  // Parallel
+    thread t3(checkProgress); // Concurrent
+    
     t1.join();
     t2.join();
     t3.join();
 }
+    
+// Activity 5
 
 void packBoxSequential() {
     cout << "\nPacking boxes sequentially...\n" << endl;
