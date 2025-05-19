95c5d6fdafde462dda18382c46de84d1a667f8ce
Jhevan Baque <aerovan.dev@gmail.com>
2025-05-19 08:09:57 +0800

    Update act3.cpp

--- act3.cpp
+++ act3.cpp
@@ -1,34 +1,29 @@
 #include <iostream>
 #include <thread>
-#include <chrono>
 using namespace std;
 
-void washDishes() {
-    string Steps[4] = {"1. Get the dishes", "2. Fill the sink", "3. Wash the dishes", "4. Rinse"};
-    for (int i = 0 ; i < 4; i++) {
-        cout << Steps[i] << endl;
+void PrintWords(int value) {
+    string Words[4] = {"Be Brave", "Fortitude", "Excellence", "Uprightness"};
+    for (int i = 1; i < 801; i++) {
+        cout << " " << Words[value] << " ";
         this_thread::sleep_for(chrono::milliseconds(200));
     }
 }
 
-void foldClothes() {
-    string Steps[3] = {"1. Lay the clothes flat", "2. Fold the sides", "3. Fold the top"};
-    for (int i = 0 ; i < 3; i++) {
-        cout << Steps[i] << endl;
-        this_thread::sleep_for(chrono::milliseconds(200));
-    }
-}
-
-void PerformHouseChores() {
-    cout << "Performing House Chores (Parallel).." << endl;
-    thread t1(washDishes);
-    thread t2(foldClothes);
+void ThreadedPrint() {
+    thread t1(PrintWords, 0);
+    thread t2(PrintWords, 1);
+    thread t3(PrintWords, 2);
+    thread t4(PrintWords, 3);
 
     t1.join();
     t2.join();
+    t3.join();
+    t4.join();
+    cout << endl << "Jhevan P. Baque";
 }
 
 int main() {
-    PerformHouseChores();
+    ThreadedPrint();
     return 0;
 }
